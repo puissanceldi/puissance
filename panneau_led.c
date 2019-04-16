0d9d2481d0859d75092a058786b1d9809a2e3153
LaetitiaDaCosta <laetitiadacosta@mailoo.org>
2019-04-16 16:01:58 +0200

    panneau encore!

--- panneau_led.c
+++ panneau_led.c
@@ -4,17 +4,39 @@ extern int start;
 extern int dep_g;
 extern int dep_d;
 extern int launch;
-int numero_ligne;
+int numero_ligne = 6;
 int position_ligne_depart;
 
 void position_finale(int position_ligne_depart, int numero_ligne)
 {
 	int x;
 	int y;
+	int j;
+	int i;
 	x = position_ligne_depart;
 	y = numero_ligne;
 	printf("le jeton sera positionné sur la colonne %d\n", x);
 	printf("le jeton sera positionné sur la ligne %d\n", y);
+
+	//impression de l'écran avec jeton laché
+	printf("I-------I");
+	for (i = 0; i <= 5 ; i++)
+	{
+		printf("\n");
+		for (j = 0; j <= 8; j++)
+		{
+			if ((j == 0) || (j == 8))
+				printf("I");
+			else if ((j == x) && (i == y-1))
+				printf("O");
+			else
+				printf(" ");
+		}
+	}
+	printf("\nIIIIIIIII");
+
+
+
 }
 
 void panneau_led()
