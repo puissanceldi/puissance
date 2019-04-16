4f007ff65410263039e1c63c0ee80c9bd32d320e
LaetitiaDaCosta <laetitiadacosta@mailoo.org>
2019-04-16 15:36:53 +0200

    panneau_led

--- panneau_led.c
+++ panneau_led.c
@@ -17,15 +17,33 @@ void position_finale(int position_ligne_depart, int numero_ligne)
 	printf("le jeton sera positionné sur la ligne %d\n", y);
 }
 
-
-
 void panneau_led()
 {
+	int i;
+	int j;
+	int k;
+
 	if(start == 1)
 	{
 		//placement jeton de départ
 		position_ligne_depart = 4;
 		printf("%d\n", position_ligne_depart);
+		printf("I---0---I");
+
+		//impression de l'écran vide
+		for (i = 0; i <= 5 ; i++)
+		{
+			printf("\n");
+			for (j = 0; j <= 8; j++)
+			{
+				if ((j == 0) || (j == 8))
+					printf("I");
+				else
+					printf(" ");
+			}
+		}
+		printf("\nIIIIIIIII");
+
 		start = 0;
 	}
 	if(dep_g == 1)
@@ -33,6 +51,32 @@ void panneau_led()
 		//déplacement du jeton à gauche
 		position_ligne_depart -= 1;
 		printf("%d\n", position_ligne_depart);
+
+		//impression du nouveau placement
+		for (k = 0; k <= 8; k++)
+		{
+			if ((k == 0) || (k == 8))
+				printf("I");
+			else if (k == position_ligne_depart)
+				printf("O");
+			else
+				printf("-");
+		}
+
+		//impression de l'écran vide
+		for (i = 0; i <= 5 ; i++)
+		{
+			printf("\n");
+			for (j = 0; j <= 8; j++)
+			{
+				if ((j == 0) || (j == 8))
+					printf("I");
+				else
+					printf(" ");
+			}
+		}
+		printf("\nIIIIIIIII");
+
 		dep_g = 0;
 	}
 	if(dep_d == 1)
@@ -40,6 +84,32 @@ void panneau_led()
 		//déplacement du jeton à droite
 		position_ligne_depart += 1;
 		printf("%d\n", position_ligne_depart);
+
+		//impression du nouveau placement
+		for (k = 0; k <= 8; k++)
+		{
+			if ((k == 0) || (k == 8))
+				printf("I");
+			else if (k == position_ligne_depart)
+				printf("O");
+			else
+				printf("-");
+		}
+
+		//impression de l'écran vide
+		for (i = 0; i <= 5 ; i++)
+		{
+			printf("\n");
+			for (j = 0; j <= 8; j++)
+			{
+				if ((j == 0) || (j == 8))
+					printf("I");
+				else
+					printf(" ");
+			}
+		}
+		printf("\nIIIIIIIII");
+
 		dep_d = 0;
 	}
 	if(launch == 1)
